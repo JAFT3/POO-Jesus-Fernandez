72bb75e5f466ba04198359f2220fc8c09d152f8a
Jesus Antonio Fernandez de la Torre <fernandezdelatorrejesusantonio@gmail.com>
2024-02-18 00:02:39 -0600

    Agregue documentacion explicando lo utilizado en el programa

--- src/edu/jesus_fdez/reto4/process/VendedorBoletos.java
+++ src/edu/jesus_fdez/reto4/process/VendedorBoletos.java
@@ -7,8 +7,15 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class VendedorBoletos {
+
+    //Declara una lista estática de objetos Boleto llamada listaPasajeros,
+    // que almacena información sobre los boletos vendidos.
     public static ArrayList<Boleto> listaPasajeros = new ArrayList<>();
 
+    //Este método se encarga de solicitar información al usuario para realizar una venta.
+    //Lee el nombre del comprador y la cantidad de boletos extra.
+    //Utiliza un bucle para recoger los nombres de los pasajeros adicionales.
+    //Llama al método generarVenta() para procesar la información ingresada.
     public static void realizarVenta(){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Nombre del comprador: ");
@@ -23,13 +30,16 @@ public class VendedorBoletos {
         }
         generarVenta(nombre,cantidadBoletos,pasajeros,listaPasajeros);
     }
-
+    //Este método crea un objeto Boleto con la información proporcionada y lo agrega a la lista listaPasajeros.
+    //Llama al método crearTicket(boleto) de la clase Ticket
     public static void generarVenta(String nombre, int cantidadBoletos, ArrayList<String> pasajeros, ArrayList<Boleto> listaPasajeros){
         Boleto boleto = new Boleto(nombre, cantidadBoletos, pasajeros);
         listaPasajeros.add(boleto);
         Ticket.crearTicket(boleto);
     }
 
+    //Este método imprime la información de la lista de pasajeros, incluyendo el comprador,
+    // los pasajeros adicionales, la cantidad total de boletos y el monto total de ventas.
     public static void mostrarListaPasajeros(){
         System.out.println("--Lista Pasajeros--");
         for (Boleto boleto : listaPasajeros){
