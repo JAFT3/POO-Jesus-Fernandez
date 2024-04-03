06c3568fea7cb7b179d1df3729adaa7121e73255
JAFT3 <fernandezdelatorrejesusantonio@gmail.com>
2024-04-02 22:34:52 -0600

    Agregado de textos al código en idiomas ingles y espanol.

---
+++ src/edu/jesus_fdez/evidencia2/ui/Idiomas.java
@@ -0,0 +1,12 @@
+package edu.jesus_fdez.evidencia2.ui;
+
+public class Idiomas {
+    public String ERROR_INESPERADO;
+
+    public String INICIO_SECION;
+    public String USUARIO;
+    public String CONTRASENA;
+    public String DATOS_INCORRECTOS;
+    public String INICIO_EXITOSO;
+    public String SALDO_PAGAR;
+}
