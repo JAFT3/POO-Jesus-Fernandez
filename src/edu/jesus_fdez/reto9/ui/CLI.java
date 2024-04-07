30b2120a44967b936ea38cce3ac95b9b93dbe5c3
JAFT3 <fernandezdelatorrejesusantonio@gmail.com>
2024-04-06 20:46:06 -0600

    Hice una pequeña correcion para que permitiera seleccionar el libro deseado.

--- src/edu/jesus_fdez/reto9/ui/CLI.java
+++ src/edu/jesus_fdez/reto9/ui/CLI.java
@@ -4,19 +4,21 @@ import java.util.Scanner;
 
 public class CLI {
     public static Idiomas idiomas = new ESP();
-    public static void showMenu(){
+
+    public static void showMenu() {
         System.out.println("------------------Idioma------------------" +
                 "\nDigite el idioma (Type the language)" +
                 "\n1- Español (ESP)" +
                 "\n2- English (ENG)" +
                 "\n------------------------------------------");
     }
-    public static void launchApp(){
+
+    public static void launchApp() {
         Scanner sc = new Scanner(System.in);
         showMenu();
         String idioma = sc.next();
 
-        switch (idioma.toUpperCase()){
+        switch (idioma.toUpperCase()) {
             case "ESP":
                 idiomas = new ESP();
                 break;
@@ -25,8 +27,9 @@ public class CLI {
                 break;
         }
         System.out.println(idiomas.MENU);
-        String libroSeleccionado = sc.nextLine();
-        switch (libroSeleccionado){
+        System.out.println("Seleccione el libro deseado (Select the desired book):");
+        String libroSeleccionado = sc.next(); // Cambiado a next() para evitar el salto de línea no deseado
+        switch (libroSeleccionado) {
             case "1":
                 System.out.println(Idiomas.LIBRO_SELECCIONADO + Idiomas.LIBRO1);
                 System.out.println(Idiomas.LISTA_PALABRAS);
