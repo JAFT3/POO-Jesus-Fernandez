3d175d58c4de3245ba0b9190e9e4b639eec9320d
JAFT3 <fernandezdelatorrejesusantonio@gmail.com>
2024-04-06 20:41:24 -0600

    Agregue la clase CLI, ahi dentro viene el menu de selecciona de idioma y la impresion de los libros y su respectiva seleccion.

---
+++ src/edu/jesus_fdez/reto9/ui/CLI.java
@@ -0,0 +1,54 @@
+package edu.jesus_fdez.reto9.ui;
+
+import java.util.Scanner;
+
+public class CLI {
+    public static Idiomas idiomas = new ESP();
+    public static void showMenu(){
+        System.out.println("------------------Idioma------------------" +
+                "\nDigite el idioma (Type the language)" +
+                "\n1- Español (ESP)" +
+                "\n2- English (ENG)" +
+                "\n------------------------------------------");
+    }
+    public static void launchApp(){
+        Scanner sc = new Scanner(System.in);
+        showMenu();
+        String idioma = sc.next();
+
+        switch (idioma.toUpperCase()){
+            case "ESP":
+                idiomas = new ESP();
+                break;
+            case "ENG":
+                idiomas = new ENG();
+                break;
+        }
+        System.out.println(idiomas.MENU);
+        String libroSeleccionado = sc.nextLine();
+        switch (libroSeleccionado){
+            case "1":
+                System.out.println(Idiomas.LIBRO_SELECCIONADO + Idiomas.LIBRO1);
+                System.out.println(Idiomas.LISTA_PALABRAS);
+                break;
+            case "2":
+                System.out.println(Idiomas.LIBRO_SELECCIONADO + Idiomas.LIBRO2);
+                System.out.println(Idiomas.LISTA_PALABRAS);
+                break;
+            case "3":
+                System.out.println(Idiomas.LIBRO_SELECCIONADO + Idiomas.LIBRO3);
+                System.out.println(Idiomas.LISTA_PALABRAS);
+                break;
+            case "4":
+                System.out.println(Idiomas.LIBRO_SELECCIONADO + Idiomas.LIBRO4);
+                System.out.println(Idiomas.LISTA_PALABRAS);
+                break;
+            case "5":
+                System.out.println(Idiomas.LIBRO_SELECCIONADO + Idiomas.LIBRO5);
+                System.out.println(Idiomas.LISTA_PALABRAS);
+                break;
+            default:
+                System.out.println(Idiomas.ERROR_LIBRO);
+        }
+    }
+}
