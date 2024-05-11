670cf07d78d7adf7191e8a71ed039112aed02d43
JAFT3 <fernandezdelatorrejesusantonio@gmail.com>
2024-05-11 02:49:19 -0600

    Se creo la clase main del programa la cual presenta el lanzamiento de la app.

--- src/edu/jesus_fdez/evidencia3/Main.java
+++ src/edu/jesus_fdez/evidencia3/Main.java
@@ -1,4 +1,12 @@
 package edu.jesus_fdez.evidencia3;
 
+
+import edu.jesus_fdez.evidencia3.data.Simbolos;
+import edu.jesus_fdez.evidencia3.ui.CLI;
+
 public class Main {
+    public static void main(String[] args){
+        //Simbolos.imprimirSimbolos();
+        CLI.launchApp();
+    }
 }
